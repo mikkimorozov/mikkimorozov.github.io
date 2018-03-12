3f802e14b7380bbe6f7da6d804e5a52724d9f49c
mikkimorozov <35310141+mikkimorozov@users.noreply.github.com>
2018-03-12 18:23:59 +0100

    My own portfolio
    My own portfolio web site which was created with using Bootstrap, some JQuory and SASS.

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -15,21 +15,3 @@ $('.container a').on('click', function(){
             $(".navbar-toggler").trigger( "click" );
         }
  });
-
-
-//E-mail Ajax Send
-$("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-        type: "POST",
-        url: "mail.php", //Change
-        data: th.serialize()
-    }).done(function() {
-        alert("Thank you!");
-        setTimeout(function() {
-            // Done Functions
-            th.trigger("reset");
-        }, 1000);
-    });
-    return false;
-});
